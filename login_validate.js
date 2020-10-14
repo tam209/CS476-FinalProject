b67c55876754d76b3843e43d16aedb36a68c8e96
torisambrook <tori.sambrook@gmail.com>
2020-10-13 20:37:35 -0600

    Update login_validate.js

--- login_validate.js
+++ login_validate.js
@@ -9,14 +9,13 @@ function validateLogin(event) {
 	var pMsg = document.getElementById("pwd_msg");
 
 	var username_v = /^[a-zA-Z0-9._-]+$/;
-  var pswd_v = /^(\S*)?\d+(\S*)?$/;
+  	var pswd_v = /^(\S*)?\d+(\S*)?$/;
 
-  if (usernameInput == null || usernameInput == ""|| !username_v.test(usernameInput)) {
+  	if (usernameInput == null || usernameInput == ""|| !username_v.test(usernameInput)) {
 	  uMsg.innerHTML = "Please enter the correct format for username. (No leading or trailing spaces)";
 	  result = false;
 	}
 
-		
 	if (pInput == null || pInput == "" || pInput.length < 8 || !pswd_v.test(pInput)) {
 	  pMsg.innerHTML = "Password must be 8 characters long, at least one non-letter character.";
 		result = false;
