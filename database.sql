886d5a91ed78192ede6f32926502aa271b0bdacd
torisambrook <tori.sambrook@gmail.com>
2020-11-03 16:35:02 -0600

    Update database.sql to match elements
    Just demonstrates the element names I used for the database.

--- database.sql
+++ database.sql
@@ -14,27 +14,20 @@ dob DATE,
 pic VARCHAR(200),
 PRIMARY KEY(user_id) );
 
-CREATE TABLE SavedBooks (
-book_id INT NOT NULL AUTO_INCREMENT, 
-user_id INT,
+CREATE TABLE Books (
+book_id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR(100),
 author VARCHAR(100),
-genre VARCHAR(100), 
-book_link VARCHAR(200),
-PRIMARY KEY(book_id), 
-FOREIGN KEY (user_id) REFERENCES Users(user_id) );
-
-/* 
-The following code is just for reference and may be used within the other files later
-
--- Insert a book into saved books
-INSERT INTO SavedBooks
-(user_id, title, author, genre, book_link)
-VALUES
-(1, "Harry Potter", "Author name", "genre type", "https://www.goodreads.com/"); // example values
+genre VARCHAR(100),
+pic VARCHAR(200),
+link VARCHAR(200),
+PRIMARY KEY(book_id) );
 
 
-Some others:
-- Retrieve matching books given a title or author or genre
-- Retrieve user from database 
-*/
+CREATE TABLE SavedBooks (
+s_book_id INT NOT NULL AUTO_INCREMENT,
+user_id INT,
+book_id INT,
+PRIMARY KEY(s_book_id),
+FOREIGN KEY(user_id) REFERENCES Users(user_id),
+FOREIGN KEY(book_id) REFERENCES Books(book_id) );
