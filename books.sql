d01bfc2c31ab1f8f47a074eaea9007ff380f3d81
torisambrook <tori.sambrook@gmail.com>
2020-11-03 16:35:20 -0600

    Create books.sql
    Contains books in the Books tables

---
+++ books.sql
@@ -0,0 +1,51 @@
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Harry Potter and the Sorcerer's Stone", "J. K. Rowling",
+"Fantasy Fiction", "books/harrypotter1.jpg", "https://www.goodreads.com/book/show/3.Harry_Potter_and_the_Sorcerer_s_Stone?from_search=true&from_srp=true&qid=AEdfykYCQL&rank=1"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Harry Potter and the Chamber of Secrets", "J. K. Rowling",
+"Fantasy Fiction", "books/harrypotter2.jpg", "https://www.goodreads.com/book/show/15881.Harry_Potter_and_the_Chamber_of_Secrets?from_search=true&from_srp=true&qid=AEdfykYCQL&rank=2"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Harry Potter and the Prisoner of Azkaban", "J. K. Rowling",
+"Fantasy Fiction", "books/harrypotter3.jpg", "https://www.goodreads.com/book/show/5.Harry_Potter_and_the_Prisoner_of_Azkaban?from_search=true&from_srp=true&qid=AEdfykYCQL&rank=4"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Harry Potter and the Goblet of Fire", "J. K. Rowling",
+"Fantasy Fiction", "books/harrypotter4.jpg", "https://www.goodreads.com/book/show/6.Harry_Potter_and_the_Goblet_of_Fire?from_search=true&from_srp=true&qid=AEdfykYCQL&rank=6"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Harry Potter and the Order of the Pheonix", "J. K. Rowling",
+"Fantasy Fiction", "books/harrypotter5.jpg", "https://www.goodreads.com/book/show/2.Harry_Potter_and_the_Order_of_the_Phoenix?from_search=true&from_srp=true&qid=AEdfykYCQL&rank=3"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Harry Potter and the Half-Blood Prince", "J. K. Rowling",
+"Fantasy Fiction", "books/harrypotter6.jpg", "https://www.goodreads.com/book/show/1.Harry_Potter_and_the_Half_Blood_Prince?from_search=true&from_srp=true&qid=AEdfykYCQL&rank=7"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Harry Potter and the Deathly Hallows", "J. K. Rowling",
+"Fantasy Fiction", "books/harrypotter7.jpg", "https://www.goodreads.com/book/show/136251.Harry_Potter_and_the_Deathly_Hallows?from_search=true&from_srp=true&qid=AEdfykYCQL&rank=10"
+);
+
+
+
