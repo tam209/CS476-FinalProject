dd4abd82284afc5f47d03d0646fad1391445acb3
torisambrook <tori.sambrook@gmail.com>
2020-11-05 15:08:51 -0600

    Update books.sql

--- books.sql
+++ books.sql
@@ -1,3 +1,4 @@
+/*harry potter books*/
 INSERT INTO Books 
 (title, author, genre, pic, link)
 VALUES
@@ -46,6 +47,95 @@ VALUES
 ("Harry Potter and the Deathly Hallows", "J. K. Rowling",
 "Fantasy Fiction", "books/harrypotter7.jpg", "https://www.goodreads.com/book/show/136251.Harry_Potter_and_the_Deathly_Hallows?from_search=true&from_srp=true&qid=AEdfykYCQL&rank=10"
 );
+
+/*best books 2019*/
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("The Testaments", "Margaret Atwood",
+"Fiction", "books/testaments.jpg", "https://www.goodreads.com/book/show/42975172-the-testaments?from_choice=true"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("The Silent Patient", "Alex Michaelide",
+"Mystery & Thriller", "books/silentpatient.jpg", "https://www.goodreads.com/book/show/40097951-the-silent-patient?from_choice=true"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Daisy Jones & The Six", "Taylor Jenkins Reid",
+"Historical Fiction", "books/daisyjones.jpg", "https://www.goodreads.com/book/show/40597810-daisy-jones-the-six?from_choice=true"
+);
+
+/*best of 2018 */
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Elevation", "Stephen King",
+"Horror", "books/elevation.jpg", "https://www.goodreads.com/book/show/38355410-elevation?from_choice=true"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Vengeful", "V.E. Schwab",
+"Science Fiction", "books/vengeful.jpg", "https://www.goodreads.com/book/show/37534835-vengeful?from_choice=true"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Kingdom of Ash", "Sarah J. Maas",
+"Young Adult Fantasy & Science Fiction", "books/kod.jpg", "https://www.goodreads.com/book/show/33590260-kingdom-of-ash?from_choice=true"
+);
+
+/*best of 2017 */
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Without Merit", "Colleen Hoover",
+"Romance", "books/withoutmerit.jpg", "https://www.goodreads.com/book/show/34684620-without-merit?from_choice=true"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Artemis", "Andy Weir",
+"Science Fiction", "books/artemis.jpg", "https://www.goodreads.com/book/show/34928122-artemis?from_choice=true"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Astrophysics for People in a Hurry", "Neil deGrasse",
+"Science & Technology", "books/astroforpeopleinahurry.jpg", "https://www.goodreads.com/book/show/32191710-astrophysics-for-people-in-a-hurry?from_choice=true"
+);
+
+/*best of 2016*/
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("Salt to the Sea", "Ruta Sepetys",
+"Young Adult Fiction", "books/salttothesea.jpg", "https://www.goodreads.com/book/show/25614492-salt-to-the-sea?from_choice=true"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("When Breath Becomes Air", "Paul Kalanithi",
+"Memoir & Autobiography", "books/whenbreathbecomesair.jpg", "https://www.goodreads.com/book/show/25614898-when-breath-becomes-air?from_choice=true"
+);
+
+INSERT INTO Books 
+(title, author, genre, pic, link)
+VALUES
+("The Hidden Oracle", "Rick Riordan",
+"Middle Grade & Children's", "books/thehiddenoracle.jpg", "https://www.goodreads.com/book/show/26252859-the-hidden-oracle?from_choice=true"
+);
+
 
 
 
